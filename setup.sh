ff7a3088424f5302353babc2608f359ae794ffa8
Jannis Isensee <jannis.isensee@gmail.com>
2021-10-30 15:44:59 +0000

    setup:correct fira code package

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ yay -Y --devel --save
 
 # PACKAGES
 yay -S --noconfirm man xclip zsh neovim picom tree feh polybar ttf-meslo-nerd-font-powerlevel10k \
-  zsh-syntaxt-highlighting zsh-autosuggestions woff-fira-code alacritty xorg-xrandr autorandr \
+  zsh-syntaxt-highlighting zsh-autosuggestions ttf-fira-code alacritty xorg-xrandr autorandr \
   pavucontrol i3-easyfocus xorg-xlsfonts gnome-screenshot libreoffice-fresh visual-studio-code-bin nvm \
   docker docker-compose zsh-history-substring-search btop
 
