d70e0b4877ce0385fe1dcce866d1eaf4caf67d90
Jannis Isensee <jannis.isensee@gmail.com>
2022-11-08 22:01:31 +0100

    nvim:remove highlight autocmd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -64,17 +64,3 @@ mnls.setup_handlers {
 }
 
 null_ls.setup()
-
-vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-  pattern = { "*.*" },
-  callback = function()
-    vim.lsp.buf.document_highlight()
-  end
-})
-
-vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
-  pattern = { "*.*" },
-  callback = function()
-    vim.lsp.buf.clear_references()
-  end
-})
