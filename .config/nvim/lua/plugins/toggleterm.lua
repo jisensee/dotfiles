b7ecc420346941fa26a9f5efdf152fb449b22fa6
Jannis Isensee <jannis.isensee@gmail.com>
2023-02-11 14:04:21 +0100

    nvim: improve toggleterm keymap

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,3 +1,7 @@
+local function uf(mapping)
+  return '<c-\\><c-N>' .. mapping
+end
+
 return {
   {
     'akinsho/toggleterm.nvim',
@@ -8,15 +12,15 @@ return {
 
       wk.register({
         name = 'Terminal',
-        t = { ':ToggleTerm size=20<cr>', 'Open terminal' },
+        t = { uf(':ToggleTerm size=20<cr>'), 'Open terminal', mode = { 'n', 't' } },
         h = { ':ToggleTerm size=20 dir="%:p:h"<cr>', 'Open terminal here' },
-        u = { '<c-\\><c-N>', 'Unfocus terminal', mode = 't' },
-        i = { '<c-\\><c-N>:resize +5<cr>i', 'Increase terminal size by a bit', mode = 't' },
-        I = { '<c-\\><c-N>:resize +10<cr>i', 'Increase terminal size by a lot', mode = 't' },
-        d = { '<c-\\><c-N>:resize -5<cr>i', 'Increase terminal size by a bit', mode = 't' },
-        D = { '<c-\\><c-N>:resize -10<cr>i', 'Decrease terminal size by a lot', mode = 't' },
-        m = { '<c-\\><c-N><c-w>_i', 'Maximize terminal window', mode = 't' },
-        r = { '<c-\\><c-N>:resize 20<cr>i', 'Restore default terminal size', mode = 't' },
+        u = { uf(''), 'Unfocus terminal', mode = 't' },
+        i = { uf(':resize +5<cr>i'), 'Increase terminal size by a bit', mode = 't' },
+        I = { uf(':resize +10<cr>i'), 'Increase terminal size by a lot', mode = 't' },
+        d = { uf(':resize -5<cr>i'), 'Increase terminal size by a bit', mode = 't' },
+        D = { uf(':resize -10<cr>i'), 'Decrease terminal size by a lot', mode = 't' },
+        m = { uf('<c-w>_i'), 'Maximize terminal window', mode = 't' },
+        r = { uf(':resize 20<cr>i'), 'Restore default terminal size', mode = 't' },
       }, { prefix = '<c-t>' })
     end
   }
