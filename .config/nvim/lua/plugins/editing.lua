722a86272965543d609cd4826a30a8808daccd82
Jannis Isensee <jannis.isensee@gmail.com>
2023-02-23 21:01:13 +0100

    nvim: add splitjoin

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -84,4 +84,20 @@ return {
       }, { prefix = '<leader>' })
     end,
   },
+  {
+    'bennypowers/splitjoin.nvim',
+    lazy = true,
+    keys = {
+      {
+        'gj',
+        function() require('splitjoin').join() end,
+        desc = 'Join the object under cursor',
+      },
+      {
+        'g,',
+        function() require('splitjoin').split() end,
+        desc = 'Split the object under cursor',
+      },
+    },
+  },
 }
