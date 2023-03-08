90e535a7e2ad74565ec64f5af87271ca5def0530
Jannis Isensee <jannis.isensee@eckcellent-it.de>
2023-03-08 15:39:34 +0100

    nvim: add snippets

--- .config/nvim/snippets/typescript.lua
+++ .config/nvim/snippets/typescript.lua
@@ -1,22 +1,42 @@
 local ls = require 'luasnip'
 local s = ls.snippet
 local i = ls.insert_node
-local fmta = require 'luasnip.extras.fmt'.fmta
+local fmta = require('luasnip.extras.fmt').fmta
 
 return {
-  s('im', fmta(
-    "import { <symbol> } from '<package>'",
-    {
+  s(
+    'im',
+    fmta("import { <symbol> } from '<package>'", {
       symbol = i(2),
       package = i(1),
-    }
-  )),
-  s('ty', fmta([[
+    })
+  ),
+  s(
+    'ty',
+    fmta(
+      [[
   type <type_name> = {
     <type_def>
   }
-  ]], {
-    type_name = i(1),
-    type_def = i(2),
-  }))
+  ]],
+      {
+        type_name = i(1),
+        type_def = i(2),
+      }
+    )
+  ),
+  s(
+    'it',
+    fmta(
+      [[
+  it('<description>', () =>> {
+    <body>
+  })
+  ]],
+      {
+        description = i(1),
+        body = i(2),
+      }
+    )
+  ),
 }
