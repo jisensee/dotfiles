599c60126e38749615fb56ecd1bda8610e9cc2a8
Jannis Isensee <jannis.isensee@gmail.com>
2023-06-27 22:23:25 +0200

    nvim: replace leap with flash

--- .config/nvim/lua/plugins/motions.lua
+++ .config/nvim/lua/plugins/motions.lua
@@ -1,9 +1,49 @@
 return {
   'chaoren/vim-wordmotion',
   {
-    'ggandor/leap.nvim',
-    config = function()
-      require 'leap'.add_default_mappings()
-    end
-  }
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+      labels = 'aoeuidhtnspg',
+      highlight = {
+        label = {
+          rainbow = {
+            enabled = true,
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        '*',
+        mode = 'n',
+        function()
+          require('flash').jump {
+            pattern = vim.fn.expand '<cword>',
+          }
+        end,
+      },
+      {
+        '<BS>',
+        mode = 'n',
+        function() require('flash').jump() end,
+      },
+      {
+        's',
+        mode = 'n',
+        function() require('flash').treesitter_search() end,
+      },
+      {
+        'S',
+        mode = 'n',
+        function() require('flash').treesitter() end,
+      },
+      {
+        'r',
+        mode = 'o',
+        function() require('flash').remote() end,
+      },
+    },
+    config = true,
+  },
 }
