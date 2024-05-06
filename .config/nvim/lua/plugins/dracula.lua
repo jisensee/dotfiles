2260b0ee069ae59ec82bc6567511877b7e5a59f4
Jannis Isensee <jannis.isensee@gmail.com>
2024-05-06 13:52:18 +0200

    nvim: fix diagnostic highlight groups

--- .config/nvim/lua/plugins/dracula.lua
+++ .config/nvim/lua/plugins/dracula.lua
@@ -1,5 +1,3 @@
-local colors = require '../colors'
-
 return {
   {
     'maxmx03/dracula.nvim',
@@ -12,28 +10,30 @@ return {
 
       dracula.setup {
         transparent = true,
-        override = {
-          DiagnosticUnderlineError = {
-            fg = 'NONE',
-            sp = colors.red,
-            underline = true,
-          },
-          DiagnosticUnderlineWarn = {
-            fg = 'NONE',
-            sp = colors.orange,
-            underline = true,
-          },
-          DiagnosticUnderlineInfo = {
-            fg = 'NONE',
-            sp = colors.cyan,
-            underline = true,
-          },
-          DiagnosticUnderlineHint = {
-            fg = 'NONE',
-            sp = colors.cyan,
-            underline = true,
-          },
-        },
+        on_highlights = function(colors)
+          return {
+            DiagnosticUnderlineError = {
+              fg = 'NONE',
+              sp = colors.red,
+              underline = true,
+            },
+            DiagnosticUnderlineWarn = {
+              fg = 'NONE',
+              sp = colors.orange,
+              underline = true,
+            },
+            DiagnosticUnderlineInfo = {
+              fg = 'NONE',
+              sp = colors.cyan,
+              underline = true,
+            },
+            DiagnosticUnderlineHint = {
+              fg = 'NONE',
+              sp = colors.cyan,
+              underline = true,
+            },
+          }
+        end,
       }
       vim.cmd.colorscheme 'dracula'
     end,
